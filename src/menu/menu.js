db415085f6f283818247b07360771a9fb8e5bc44
Dutaud <boris.dutaud@isen.yncrea.fr>
2019-08-09 10:49:54 +0200

    first commit

---
+++ src/menu/menu.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Menu extends React.Component {
+    render() {
+        return(
+            <nav>
+                <ul>
+                    <li><a href="http://www.google.fr" target="_blank">Pokédex</a></li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default Menu;
---
+++ src/paragraphe/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Paragraphe extends React.Component{
+    render(){
+        return(
+            <p>Pokédex</p>
+        );
+    }
+}
+export default Paragraphe;
